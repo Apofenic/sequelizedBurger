15dd28460f46499e24ea667e6d1f75c6e54e6137
Anthony Lubrino <anthony1@Anthonys-MacBook-Pro-2.local>
2017-05-06 22:54:47 -0400

    heroku fix

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
 var app = express();
 var db = require("./models");
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3306;
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(__dirname + "/public"));
